ad3d477d041f8adbfb156652d300403b41e18da1
Paul Chambers <paultimothychambers@gmail.com>
2021-07-09 07:06:11 -0600

    Create functions for testing

--- src/book.js
+++ src/book.js
@@ -1,12 +1,56 @@
-function createTitle() {
-  
-}
+function saveReview(review, reviews) {
+  for (var i = 0; i <= reviews.length; i++) {
+    if (review !== reviews[i]) {
+      reviews.push(review)
+      return reviews
+    } else {
+      return reviews
+    }
+  }
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  var newCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+  return newCharacter
+};
+
+function createTitle(title) {
+  title = `The ${title}`
+  return title
+};
+
+function calculatePageCount(title) {
+  var currentPageCount = 0;
+  for (let x in title) {
+    currentPageCount += 20;
+  }
+  return currentPageCount;
+};
+
+function writeBook(title, character, genre) {
+  var book = {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  }
+  return book
+};
+
+function editBook(book) {
+  book.pageCount = book.pageCount * 0.75
+  return book.pageCount
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
-}
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
+};
