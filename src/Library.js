540ee3028fddc256fc9d531dceaea97aab0d0307
Paul Chambers <paultimothychambers@gmail.com>
2021-07-11 19:43:00 -0600

    Create functions for testing

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,27 @@
+function createLibrary(lbry) {
+  var newLbry = {
+    name: lbry,
+    shelves: {fantasy: [], fiction: [], nonFiction: []}
+  }
+  return newLbry
+};
 
+function addBook(lbry, book) {
+  lbry.shelves[book.genre].unshift(book)
+};
+
+function checkoutBook(lbry, title, genre) {
+  for (var i = 0; i < lbry.shelves[genre].length; i++) {
+    if (lbry.shelves[genre][i].title === title) {
+      lbry.shelves[genre].splice(i, 1)
+      return `You have now checked out ${title} from the ${lbry.name}`
+    }
+  }
+  return `Sorry, there are currently no copies of ${title} available at the ${lbry.name}`
+};
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,42 @@
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
+};
+
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+    return shelf;
+  }
+};
+
+function unshelfBook(book, shelf) {
+  for (var i in shelf) {
+    if (shelf[i].title === book) {
+      shelf.splice(shelf.indexOf(shelf[i]), 1)
+      return shelf
+    }
+  }
+};
+
+function listTitles(shelf) {
+  var titleListing = []
+  for (i = 0; i < shelf.length; i++) {
+    titleListing.push(shelf[i].title)
+  }
+  return titleListing.join(", ")
+};
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      var itDoes = true
+    } else {
+      var itDoes = false
+    }
+  }
+  return itDoes
 };
